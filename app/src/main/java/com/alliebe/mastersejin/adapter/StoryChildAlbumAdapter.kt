3d506c6d037ae04145801d1cf31456b7aa2b059f
Sejin <sejigner@gmail.com>
2021-02-16 16:40:11 +0900

    StoryEditActivity implemented with Databinding

--- app/src/main/java/com/alliebe/mastersejin/adapter/StoryChildAlbumAdapter.kt
+++ app/src/main/java/com/alliebe/mastersejin/adapter/StoryChildAlbumAdapter.kt
@@ -10,19 +10,22 @@ import com.alliebe.mastersejin.databinding.ChildAlbumItemBinding
 import com.alliebe.mastersejin.adapter.StoryChildAlbumAdapter.ViewHolder
 import com.alliebe.mastersejin.dtos.StoryChildAlbumDTO
 
-class StoryChildAlbumAdapter(private val childAlbumData: List<StoryChildAlbumDTO>?) : RecyclerView.Adapter<ViewHolder>() {
+class StoryChildAlbumAdapter(private val childAlbumData: List<StoryChildAlbumDTO>?) :
+    RecyclerView.Adapter<ViewHolder>() {
 
-    inner class ViewHolder(private val itemView : View, val binding: ChildAlbumItemBinding) :
-            RecyclerView.ViewHolder(itemView)
+    inner class ViewHolder(private val itemView: View, val binding: ChildAlbumItemBinding) :
+        RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = DataBindingUtil.inflate(LayoutInflater.from(parent.context),
-        R.layout.child_album_item, parent, false) as ChildAlbumItemBinding
+        val binding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.child_album_item, parent, false
+        ) as ChildAlbumItemBinding
         return ViewHolder(binding.root, binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        if((childAlbumData?.size ?: 0) > position)
+        if ((childAlbumData?.size ?: 0) > position)
             holder.binding.data = childAlbumData?.get(position)
 
         holder.binding.executePendingBindings()
